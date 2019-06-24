191ad80759c2e2ce4102a1486cb001b897e6a48e
Simon Biggs <me@simonbiggs.net>
2019-06-25 05:21:44 +1000

    initial commit of app template

---
+++ app/src/root.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+  Classes
+} from '@blueprintjs/core';
+
+
+interface IAppRootProps { }
+interface IAppRootState extends Readonly<{}> { }
+
+class AppRoot extends React.Component<IAppRootProps, IAppRootState> {
+  render() {
+    return (
+      <div className="AppRoot">
+
+      </div>
+    );
+  }
+}
+
+export default AppRoot;
+
+
+
