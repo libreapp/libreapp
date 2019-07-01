7d1225d790be7db4c70cb990388e9848bb53c419
Simon Biggs <me@simonbiggs.net>
2019-07-01 22:38:46 +1000

    dicom header now printing on file added to directory

--- app/src/root.tsx
+++ app/src/root.tsx
@@ -12,6 +12,7 @@ interface IAppRootState extends Readonly<{}> { }
 
 declare let languagePluginLoader: any;
 declare let pyodide: any;
+declare let Module: any;
 
 class AppRoot extends React.Component<IAppRootProps, IAppRootState> {
   render() {
@@ -23,13 +24,31 @@ class AppRoot extends React.Component<IAppRootProps, IAppRootState> {
   }
 }
 
-languagePluginLoader.then(() => {
+const pythonReady = languagePluginLoader.then(() => {
   return Promise.all([
     pyodide.loadPackage(['matplotlib', 'numpy', 'pandas']),
     pyodide.runPython(loadWheels)
   ])
 })
 
-let socket = new WebSocket("ws://localhost:8000/api");
+let socket = new WebSocket("ws://127.0.0.1:29285/api");
+
+socket.onmessage = (event) => {
+  console.log(event)
+
+  const messageData: Blob = event.data
+  const arrayBuffer = new Response(messageData).arrayBuffer();
+
+  Promise.all([arrayBuffer, pythonReady]).then(([array,]) => {
+    Module['FS_createDataFile']('/', 'temp.dcm', new Uint8Array(array), true, true, true);
+
+    const header: string = pyodide.runPython(`
+import pydicom
+repr(pydicom.dcmread('temp.dcm', force=True))`)
+
+    const root = document.getElementsByClassName("AppRoot")[0];
+    root.innerHTML = header;
+  })
+}
 
 export default AppRoot;
