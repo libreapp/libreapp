d384451691c5548a2418c87aab8be50bfc771ac0
Simon Biggs <me@simonbiggs.net>
2019-07-01 23:09:43 +1000

    improve download script

--- app/public/vendor/download.py
+++ app/public/vendor/download.py
@@ -9,16 +9,16 @@ import json
 
 FILES_TO_DOWNLOAD = [
     {
-        "url": 'https://github.com/iodide-project/pyodide/releases/download/0.13.0/pyodide-build-0.13.0.tar.bz2',
-        "hash": 'd8bb9ec31c87d80bcc4ed9f1477289b679b03ba4a082ebddde88f9416a92376a',
-        "location": '.',
-        "extract": True
-    },
-    {
         "url": 'https://files.pythonhosted.org/packages/97/ae/93aeb6ba65cf976a23e735e9d32b0d1ffa2797c418f7161300be2ec1f1dd/pydicom-1.2.0-py2.py3-none-any.whl',
         "hash": '2132a9b15a927a1c35a757c0bdef30c373c89cc999cf901633dcd0e8bdd22e84',
         "location": 'wheels',
         "extract": False
+    },
+    {
+        "url": 'https://github.com/iodide-project/pyodide/releases/download/0.13.0/pyodide-build-0.13.0.tar.bz2',
+        "hash": 'd8bb9ec31c87d80bcc4ed9f1477289b679b03ba4a082ebddde88f9416a92376a',
+        "location": '.',
+        "extract": True
     }
 ]
 
@@ -27,12 +27,13 @@ FILES_TO_DOWNLOAD = [
 def main():
     for item in FILES_TO_DOWNLOAD:
         filepath, headers = urllib.request.urlretrieve(item['url'])
-        print(headers)
+        # print(headers)
         # TODO check hash
         filename = item['url'].split('/')[-1]
 
         Path(item['location']).mkdir(exist_ok=True)
-        new_location = os.path.join(item['location'], filename)
+        new_location = os.path.abspath(
+            os.path.join(item['location'], filename))
 
         shutil.move(filepath, new_location)
 
